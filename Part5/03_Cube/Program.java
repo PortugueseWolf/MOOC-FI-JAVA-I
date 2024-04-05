bd10248a06005d3b81c19254ac4c802e27bb08e1
PortugueseWolf <danieljdferreira@hotmail.com>
2024-04-05 14:22:56 +0100

    Done

---
+++ Part5/03_Cube/Program.java
@@ -0,0 +1,10 @@
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Cube cube = new Cube(4);
+        System.out.println(cube.volume());
+        System.out.println(cube);
+    }
+}
