67be97c065eadec9c9535d0decdcc794be0d207b
PortugueseWolf <danieljdferreira@hotmail.com>
2024-04-10 12:44:36 +0100

    Done

---
+++ Part6/10_TodoList/UserInterface.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    private Scanner scanner;
+    private TodoList list;
+
+    public UserInterface(TodoList list, Scanner scanner){
+        this.scanner = scanner;
+        this.list = list;
+    }
+
+    public void start(){
+        while(true){
+            System.out.print("Command: ");
+            String command = scanner.nextLine();
+            if(command.equals("stop")){
+                scanner.close();
+                break;
+            }
+            
+            if(command.equals("add")){
+                System.out.print("To add: ");
+                String task = scanner.nextLine();
+                list.add(task);
+            }
+
+            if(command.equals("list")){
+                list.print();
+            }
+
+            if(command.equals("remove")){
+                System.out.print("Which one is removed? ");
+                int remove = Integer.valueOf(scanner.nextLine());
+                list.remove(remove);
+            }
+        }
+    }
+}
