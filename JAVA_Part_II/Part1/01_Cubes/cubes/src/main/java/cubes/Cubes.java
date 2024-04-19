020dcf5d6cb38cad2029bb290cec4c51a82a9468
PortugueseWolf <danieljdferreira@gmail.com>
2024-04-19 16:39:05 +0100

    Updated to include JAVA 2

---
+++ JAVA_Part_II/Part1/01_Cubes/cubes/src/main/java/cubes/Cubes.java
@@ -0,0 +1,19 @@
+package cubes;
+
+import java.util.Scanner;
+
+public class Cubes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                scanner.close();
+                break;
+            }
+            int number = Integer.valueOf(input);
+            System.out.println(number * number * number);
+        }
+    }
+}
---
+++ JAVA_Part_II/Part1/01_Cubes/cubes/src/test/java/cubes/CubesTest.java
@@ -0,0 +1,20 @@
+package cubes;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CubesTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
