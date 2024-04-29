09103d19adb3300663d4a375c3247ab58f04970d
PortugueseWolf <125361522+PortugueseWolf@users.noreply.github.com>
2024-04-29 11:24:39 +0000

    Folder renaming and exercise Warehousing in development

---
+++ JAVA_Part_II/Part9/03_Warehousing/warehousing/src/main/java/warehousing/ChangeHistory.java
@@ -0,0 +1,24 @@
+package warehousing;
+
+import java.util.ArrayList;
+
+public class ChangeHistory {
+    private ArrayList<Double> history;
+
+    public ChangeHistory() {
+        this.history = new ArrayList<>();
+    }
+
+    public void add(double status) {
+        history.add(status);
+    }
+
+    public void clear() {
+        history.clear();
+    }
+
+    public String toString() {
+        return history.toString();
+    }
+    
+}
---
+++ JAVA_Part_II/Part9/03_Warehousing/warehousing/src/main/java/warehousing/Main.java
@@ -0,0 +1,8 @@
+package warehousing;
+
+public class Main {
+    public static void main(String[] args) {
+        ProductWarehouse juice = new ProductWarehouse("juice", 1000.0);
+        System.out.println(juice);
+    }
+}
