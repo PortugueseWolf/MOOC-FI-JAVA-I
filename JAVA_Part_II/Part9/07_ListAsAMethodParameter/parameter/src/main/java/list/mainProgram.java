b06f60075057a869cfece4cbd9c29c338d967246
PortugueseWolf <danieljdferreira@gmail.com>
2024-04-30 11:32:24 +0100

    Done

---
+++ JAVA_Part_II/Part9/07_ListAsAMethodParameter/parameter/src/main/java/list/mainProgram.java
@@ -0,0 +1,13 @@
+package list;
+
+import java.util.*;
+
+public class mainProgram {
+    public static void main(String[] args) {
+
+    }
+
+    public static int returnSize(List<String> list) {
+        return list.size();
+    }
+}
---
+++ JAVA_Part_II/Part9/07_ListAsAMethodParameter/parameter/src/test/java/list/mainProgramTest.java
@@ -0,0 +1,13 @@
+package list;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class mainProgramTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
