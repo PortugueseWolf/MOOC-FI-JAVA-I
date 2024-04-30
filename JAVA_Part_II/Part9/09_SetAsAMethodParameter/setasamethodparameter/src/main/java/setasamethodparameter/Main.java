039b92dc683a49ca1c50631959ad9ce9eef791ec
PortugueseWolf <danieljdferreira@gmail.com>
2024-04-30 11:50:47 +0100

    Done

---
+++ JAVA_Part_II/Part9/09_SetAsAMethodParameter/setasamethodparameter/src/main/java/setasamethodparameter/Main.java
@@ -0,0 +1,13 @@
+package setasamethodparameter;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int returnSize(Set<String> set) {
+        return set.size();
+    }
+}
---
+++ JAVA_Part_II/Part9/09_SetAsAMethodParameter/setasamethodparameter/src/test/java/setasamethodparameter/MainTest.java
@@ -0,0 +1,13 @@
+package setasamethodparameter;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
