e9ffa2f09b43aa4e8fbeb0ab4b56a75b5f8b7631
PortugueseWolf <danieljdferreira@gmail.com>
2024-05-01 19:08:05 +0100

    Done

---
+++ JAVA_Part_II/Part10/03_PositiveNumbers/positivenumbers/src/test/java/positivenumbers/PositiveNumbersTest.java
@@ -0,0 +1,13 @@
+package positivenumbers;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PositiveNumbersTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
