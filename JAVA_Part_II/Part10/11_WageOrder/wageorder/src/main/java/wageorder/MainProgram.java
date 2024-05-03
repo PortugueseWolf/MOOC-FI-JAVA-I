5b6a1a1753455b9efb621b093e90f7011a81a31b
PortugueseWolf <danieljdferreira@gmail.com>
2024-05-03 13:44:55 +0100

    Done

--- JAVA_Part_II/Part10/11_WageOrder/wageorder/src/main/java/wageorder/MainProgram.java
+++ JAVA_Part_II/Part10/11_WageOrder/wageorder/src/main/java/wageorder/MainProgram.java
@@ -1,9 +1,21 @@
 package wageorder;
 
+import java.util.Collections;
 import java.util.ArrayList;
 
 public class MainProgram {
     public static void main(String[] args) {
-        ArrayList<Human> list = new ArrayList<>
+        ArrayList<Human> humans = new ArrayList<>();
+        humans.add(new Human("Matti", 150000));
+        humans.add(new Human("Merja", 500));
+        humans.add(new Human("Pertti", 80));
+
+        System.out.println(humans);
+
+        /*
+         * Uncomment the comment below when you have completed the compareTo-method.
+         */
+        Collections.sort(humans);
+        System.out.println(humans);
     }
 }
