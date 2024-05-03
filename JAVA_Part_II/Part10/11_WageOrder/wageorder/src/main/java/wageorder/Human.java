e4d5aeecb873f05cd008be42d915d3ca4fa078ce
PortugueseWolf <125361522+PortugueseWolf@users.noreply.github.com>
2024-05-03 08:02:06 +0000

    In progress

--- JAVA_Part_II/Part10/11_WageOrder/wageorder/src/main/java/wageorder/Human.java
+++ JAVA_Part_II/Part10/11_WageOrder/wageorder/src/main/java/wageorder/Human.java
@@ -1,6 +1,6 @@
 package wageorder;
 
-public class Human {
+public class Human implements Comparable<Human>{
 
     private int wage;
     private String name;
@@ -22,5 +22,19 @@ public class Human {
     @Override
     public String toString() {
         return name + " " + wage;
+    }
+
+    @Override
+    public int compareTo(Human human) {
+        if(this.wage == human.getWage()) {
+            return 0;
+        }
+        if(this.wage > human.getWage()) {
+            return -1;
+        }
+        if (this.wage < human.getWage()) {
+            return 1;
+        }
+        return -1;
     }
 }
--- JAVA_Part_II/Part10/11_WageOrder/wageorder/src/main/java/wageorder/MainProgram.java
+++ JAVA_Part_II/Part10/11_WageOrder/wageorder/src/main/java/wageorder/MainProgram.java
@@ -1,8 +1,9 @@
 package wageorder;
 
+import java.util.ArrayList;
 
 public class MainProgram {
     public static void main(String[] args) {
-        
+        ArrayList<Human> list = new ArrayList<>
     }
 }
