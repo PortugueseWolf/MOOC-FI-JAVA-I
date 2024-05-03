b77e256ba06487ae787fd7350d0b30a03eea239d
PortugueseWolf <danieljdferreira@gmail.com>
2024-05-03 13:59:16 +0100

    Done

---
+++ JAVA_Part_II/Part10/12_StudentsInAlphabeticalOrder/students/src/test/java/students/MainProgramTest.java
@@ -0,0 +1,12 @@
+package students;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class MainProgramTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
